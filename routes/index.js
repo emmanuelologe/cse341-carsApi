8334aa53770f42d057da8255d8c7588a7b56729b
emmanuelologe <epicraez>
2025-11-22 23:55:02 +0100

    Cars Api swagger

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/cars', require('./cars'));
-/* router.use('/', require('./swagger')); */
+router.use('/', require('./swagger'));
 
 module.exports = router;
